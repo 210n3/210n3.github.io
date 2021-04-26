be0ad7c15048772f7cd1631c3d1ddefa9ff89670
210n3 <77115470+210n3@users.noreply.github.com>
2021-04-26 10:32:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
 echo "working"
+parted --script /dev/$DISK \
+        mklabel gpt \
+        mkpart ESP fat32 1MiB 200MiB \
+        set 1 boot on \
+        name 1 efi \
+        mkpart primary 200MiB 800MiB \
+        name 2 boot \
+        mkpart primary 800MiB 32Gib \
+        name 3 swap \
+        mkpart primary 32GiB 100% \
+        name 3 btrfs \
+        print \
+        quit
