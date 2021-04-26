b3767532503e9a3e916cc7e87ca4c9c960f062fc
210n3 <77115470+210n3@users.noreply.github.com>
2021-04-26 10:33:33 +0530

    Update and rename test to test.sh

---
+++ test.sh
@@ -0,0 +1,1 @@
+sed -i 's/HOOKS=(base\ udev\ autodetect\ modconf\ block\ filesystems\ keyboard\ fsck)/HOOKS="base\ udev\ autodetect\ modconf\ block\ encrypt\ filesystems\ keyboard\ fsck"/' /etc/mkinitcpio.conf
